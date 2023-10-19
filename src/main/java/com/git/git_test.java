847e2eeff1e13e596bfacbb1c3f1738533fc2874
Wang <2741769265@qq.com>
2023-10-19 08:16:54 +0800

    first commit

---
+++ src/main/java/com/git/git_test.java
@@ -0,0 +1,9 @@
+package com.git;
+
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+    }
+}
