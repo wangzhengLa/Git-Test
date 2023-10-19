5d1f646ed8822413cd0f012d90cd29a2e31ffcba
Wang <2741769265@qq.com>
2023-10-19 08:17:49 +0800

    second commit

--- src/main/java/com/git/git_test.java
+++ src/main/java/com/git/git_test.java
@@ -4,6 +4,5 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
-        System.out.println("hello3");
     }
 }
