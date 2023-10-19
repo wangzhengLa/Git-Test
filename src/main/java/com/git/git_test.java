1572bf08864efb4fc9f8584ce6acb4736578d550
wangzhengLa <104749393+wangzhengLa@users.noreply.github.com>
2023-10-19 08:55:42 +0800

    pull commit

--- src/main/java/com/git/git_test.java
+++ src/main/java/com/git/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("test push");
+        System.out.println("test pull");
     }
 }
